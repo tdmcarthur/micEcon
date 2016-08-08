3d4215d0ab32063c0eb203973e6f631ca8b3f967
tdmcarthur <travis.d.mcarthur@gmail.com>
2016-08-07 20:22:58 -0500

    Removed stringr example of testthat()

--- pkg/micEconIndex/tests/testthat/testEKScorrection.R
+++ pkg/micEconIndex/tests/testthat/testEKScorrection.R
@@ -1,14 +1,14 @@
 
 library(micEconIndex)
-library(stringr)
+# library(stringr)
 context("EKS transitivity correction")
 
 
-test_that("str_length is number of characters", {
-  expect_equal(str_length("a"), 1)
-  expect_equal(str_length("ab"), 2)
-  expect_equal(str_length("abc"), 3)
-})
+# test_that("str_length is number of characters", {
+#   expect_equal(str_length("a"), 1)
+#   expect_equal(str_length("ab"), 2)
+#   expect_equal(str_length("abc"), 3)
+# })
 
 
 priceQuantMat <- read.csv("~/svn/micEcon/pkg/micEconIndex/tests/testthat/priceQuantMat.txt")
